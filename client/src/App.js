1d0d0e949379e8759d9bb361d6b4d169d6706514
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2024-02-03 13:26:33 -0800

    updated react app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import BibNumberInput from './components/BibNumberInput/index';
 import ActiveRunnerList from './components/ActiveRunnerList';
+import CheckOutForm from './components/CheckOut';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <BibNumberInput />
+      <CheckOutForm />
       <ActiveRunnerList />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
