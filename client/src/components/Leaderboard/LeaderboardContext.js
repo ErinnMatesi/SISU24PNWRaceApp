0bb99cb7bd49fa82ddbbf4b10f13347df1b5413f
ErinnMatesi <erinnmatesi@gmail.com>
2024-05-08 09:21:47 -0700

    added styling to leaderboard and adjusted fetch headers to not use cache on refresh

--- client/src/components/Leaderboard/LeaderboardContext.js
+++ client/src/components/Leaderboard/LeaderboardContext.js
@@ -7,6 +7,11 @@ export const LeaderboardProvider = ({ children }) => {
     const [femaleRunners, setFemaleRunners] = useState([]);
     const [teams, setTeams] = useState([]);
     const [hundredMilers, setHundredMilers] = useState([]);
+    const fetchSettings = {
+        headers: {
+            'Cache-Control': 'no-cache, no-store, must-revalidate' // Directs the browser to fetch a fresh version
+        }
+    };
 
     useEffect(() => {
         fetchMaleRunners();
@@ -16,28 +21,28 @@ export const LeaderboardProvider = ({ children }) => {
     }, []);
 
     const fetchMaleRunners = () => {
-        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/gender/Male`)
+        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/gender/Male`, fetchSettings)
             .then(response => response.json())
             .then(data => setMaleRunners(data))
             .catch(error => console.error('Error fetching male runners:', error));
     };
 
     const fetchFemaleRunners = () => {
-        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/gender/Female`)
+        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/gender/Female`, fetchSettings)
             .then(response => response.json())
             .then(data => setFemaleRunners(data))
             .catch(error => console.error('Error fetching female runners:', error));
     };
 
     const fetchTeams = () => {
-        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/teams`)
+        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/teams`, fetchSettings)
             .then(response => response.json())
             .then(data => setTeams(data))
             .catch(error => console.error('Error fetching teams:', error));
     };
 
     const fetchHundredMilers = () => {
-        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/100milers`)
+        fetch(`${process.env.REACT_APP_API_URL}/leaderboard/100milers`, fetchSettings)
             .then(response => response.json())
             .then(data => setHundredMilers(data))
             .catch(error => console.error('Error fetching 100 milers:', error));
